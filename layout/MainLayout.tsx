7020853057bb77d396491d8ee28542ac8d879660
Lyshka <53864647+Lyshka@users.noreply.github.com>
2024-01-06 16:51:59 +0300

    add
    add analytic

--- layout/MainLayout.tsx
+++ layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import NextTopLoader from "nextjs-toploader";
 import { ReactNode } from "react";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header/Header";
@@ -15,6 +16,7 @@ export const MainLayout = ({ children }: IMainLayout) => {
   return (
     <div className="min-h-screen max-w-full">
       <SpeedInsights />
+      <Analytics />
       <NextTopLoader />
       <ScriptGeneral />
       <AlertSuccess />
