00d4f0e1b074498c100c03c5975fc4c9dd8f4d39
Lyshka <53864647+Lyshka@users.noreply.github.com>
2024-08-28 21:25:56 +0300

    add slider project

--- components/Projects/CardProject.tsx
+++ components/Projects/CardProject.tsx
@@ -1,40 +1,46 @@
 "use client";
 
+import { Categories } from "@/constants/projects";
 import Image, { StaticImageData } from "next/image";
-import { useState } from "react";
-import { ImgZoomPopup } from "../Popup/ImgZoomPopup";
 
 interface ICardProject {
   img: StaticImageData;
+  tags: string[];
+  title: string;
+  category: Categories;
 }
 
-export const CardProject = ({ img }: ICardProject) => {
-  const [open, setOpen] = useState(false);
-
-  const onOpen = () => {
-    setOpen((prv) => !prv);
-  };
-
+export const CardProject = ({ img, tags, title, category }: ICardProject) => {
   return (
-    <div className="shadow-2xl 2xl:block contents">
-      <Image
-        className="2xl:block hidden cursor-pointer"
-        width={417}
-        height={416}
-        src={img}
-        alt="project"
-        onClick={onOpen}
-      />
-      <Image
-        className="2xl:hidden inline-block cursor-pointer"
-        width={272}
-        height={272}
-        src={img}
-        alt="project"
-        onClick={onOpen}
-      />
+    <div className="bg-[#F5F5F5] xl:rounded-[32px] rounded-2xl xl:pt-7 pt-4 xl:h-[592px] h-96 flex flex-col items-center justify-between xl:px-7 px-5 relative">
+      <span className="absolute bg-[#BD2D2D] top-8 left-0 xl:p-1.5 p-1 xl:pl-6 pl-2.5 text-white xl:text-xl text-xs rounded-r-lg xl:leading-[21.72px] leading-[15.3px]">{category}</span>
 
-      {open && <ImgZoomPopup onOpen={onOpen} img={img} />}
+      <div className="flex flex-col items-center gap-4">
+        <p className="xl:text-[36px] text-xl xl:leading-[43px] text-center font-medium text-[#333333]">
+          {title}
+        </p>
+
+        <ul className="xl:max-w-[312px] max-w-[164px] w-full flex flex-wrap gap-1 justify-between">
+          {tags.map((tag, idx) => (
+            <li
+              className="bg-white xl:rounded-[20px] rounded-[10px] flex-1 xl:px-3.5 px-2 xl:py-1.5 py-0.5 xl:text-sm text-[10px] xl:leading-[20.56px] font-medium text-center"
+              key={idx}
+            >
+              {tag}
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="xl:h-[381px] h-[200px] w-full flex justify-center items-center">
+        <Image
+          src={img}
+          alt="Кейс"
+          className="w-full h-full object-contain"
+          width={360}
+          height={381}
+        />
+      </div>
     </div>
   );
 };
--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 import { useContext, useEffect, useState } from "react";
 import { isMobile } from "react-device-detect";
 
@@ -14,29 +16,9 @@ export const Projects = () => {
   const { categoryProjects } = useContext(MainContext);
 
   const [filteredProjects, setFilteredProjects] = useState(projects);
-  const [projectInfo, setProjectInfo] = useState({
-    length: 6,
-    active: false,
-  });
 
   const onDownloadPortfolio = () => {
     window.location.href = `/portf.pptx`;
-  };
-
-  const onShowFullPost = () => {
-    if (projectInfo.active) {
-      return setProjectInfo((prv) => ({
-        ...prv,
-        length: 6,
-        active: !prv.active,
-      }));
-    }
-
-    setProjectInfo((prv) => ({
-      ...prv,
-      length: filteredProjects.length,
-      active: !prv.active,
-    }));
   };
 
   useEffect(() => {
@@ -50,44 +32,43 @@ export const Projects = () => {
     setFilteredProjects(filtered);
   }, [categoryProjects]);
 
-  useEffect(() => {
-    if (isMobile) {
-      setProjectInfo((prv) => ({
-        ...prv,
-        length: filteredProjects.length,
-        active: !prv.active,
-      }));
-    }
-  }, [filteredProjects.length]);
-
   return (
     <section id="portfolio" className="flex justify-center items-center">
       <div className="container 2xl:py-[60px] py-6 flex flex-col 2xl:gap-10 gap-4">
-        <div className="flex 2xl:flex-row flex-col items-center justify-between w-full gap-4">
+        <div className="flex flex-col items-center w-full gap-4">
           <Title text="Проекты" />
 
           <SelectedMenu />
         </div>
 
-        <div className="flex justify-center items-center">
-          <div className="2xl:grid flex 2xl:overflow-auto 2xl:grid-cols-3 2xl:gap-6 gap-4 justify-start items-start overflow-y-scroll">
-            {filteredProjects
-              .slice(0, projectInfo.length)
-              .map(({ id, img }) => (
-                <CardProject key={id} img={img} />
-              ))}
-          </div>
+        <div className="flex justify-center items-center xl:max-w-[1300px] w-full">
+          <Swiper
+            className="w-full"
+            breakpoints={{
+              1368: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+              },
+              0: {
+                width: 260,
+                spaceBetween: 16,
+              },
+            }}
+          >
+            {filteredProjects.map(({ id, img, tags, title, category }) => (
+              <SwiperSlide key={id}>
+                <CardProject
+                  img={img}
+                  tags={tags}
+                  title={title}
+                  category={category}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
 
-        <div className="flex xl:gap-6 gap-5 xl:flex-row flex-col">
-          {filteredProjects.length > 6 && (
-            <Button onClick={onShowFullPost} className="2xl:block hidden">
-              {projectInfo.active ? "Свернуть" : "Смотреть все проекты"}
-            </Button>
-          )}
-
-          <Button onClick={onDownloadPortfolio}>Скачать портфолио</Button>
-        </div>
+        <Button onClick={onDownloadPortfolio}>Скачать портфолио</Button>
       </div>
     </section>
   );
